22246661552a7b1343de718ae53db92267c15836
J3r3mC <j.corbeilla@outlook.fr>
2026-07-11 16:27:52 +0200

    ADD structure

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     globals: true,
     coverage: {
       provider: "v8",
-      reporter: ["text", "html", "lcov"],
+      reporter: ["text", "text-summary", "html", "lcov"],
       reportsDirectory: "./coverage",
       include: ["src/**/*.ts"],
       exclude: ["src/__tests__/**"],
